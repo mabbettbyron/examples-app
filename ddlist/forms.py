8d174e383b34324ee2824d257e45f0c5d2f76d00
mabbettbyron <mabznz@gmail.com>
2019-11-13 17:30:48 +1300

    Next and previous buttons sort of working

--- ddlist/forms.py
+++ ddlist/forms.py
@@ -2,13 +2,17 @@ from django import forms
 from .models import Reading, Country, City
 
 class ReadingForm(forms.ModelForm):
+
+    city = forms.ModelChoiceField(queryset=City.objects.all(), widget=forms.Select())
     country = forms.ModelChoiceField(queryset=Country.objects.all(), widget=forms.Select())
-    
+
+
     class Meta:
         model = Reading
-        fields = ('name', 'birthdate', 'city')
+        fields = ()
 
     # Override
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['city'].queryset = City.objects.none()
+        #self.fields['city'].queryset = City.objects.none()
+        #self.fields['country'].queryset = Country.objects.none()
